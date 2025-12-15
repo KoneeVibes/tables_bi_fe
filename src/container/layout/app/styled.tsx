217e29be25b329e85e840ed887a1477f2d2ea4bb
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-12-16 00:40:34 +0100

    fix

--- src/container/layout/app/styled.tsx
+++ src/container/layout/app/styled.tsx
@@ -3,7 +3,6 @@ import { Container, styled } from "@mui/material";
 export const AppLayoutWrapper = styled(Container)(() => {
 	return {
 		padding: 0,
-		width: "auto",
 		overflow: "hidden",
 		backgroundColor: "var(--light-color)",
 	};
